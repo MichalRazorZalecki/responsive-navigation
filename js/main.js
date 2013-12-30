196ad0622b1b1f5f9e88cce0a2629633a0f36fe1
MichalRazorZalecki <zalecki.post@gmail.com>
2013-12-30 20:11:03 +0100

    v 2.2

--- js/main.js
+++ js/main.js
@@ -7,10 +7,41 @@ if(window.navigator.userAgent.indexOf("Windows Phone") > 0)
 //$('html').addClass('touch'); //Uncoment to pretend device witch touch screen
 
 /* ==========================================================================
+   navShow & navHide
+   ========================================================================== */
+
+var getUnqueuedOpts = function (opts) {
+    return {
+        queue: false,
+        duration: opts.duration,
+        easing: opts.easing
+    };
+};
+$.fn.navShow = function (opts) {
+    if (!opts) {
+        opts = {};
+    }
+    $(this).css('opacity', 0).slideDown(opts).animate({
+            opacity: 1
+        }, getUnqueuedOpts(opts));
+};
+$.fn.navHide = function (opts) {
+    if (!opts) {
+        opts = {};
+    }
+    $(this).css('opacity', 1).slideUp(opts).animate({
+            opacity: 0
+        }, getUnqueuedOpts(opts));
+};
+
+/* ==========================================================================
    Menu Responsive Dropdown
    ========================================================================== */
 
 var nav_has_sub_menus = $('#menu-rd .has-sub-menu');
+
+var animationSpeed = 500;
+var hideSubMenuDelay = 300; //0 for closing immediately
 
 nav_has_sub_menus.each(function(index, element){
 		
@@ -18,39 +49,42 @@ nav_has_sub_menus.each(function(index, element){
 	if($('html').hasClass('touch')){
 		$(element).find('> .show-sub-menu').on('click', function(){
 			if($(element).find('> .sub-menu').hasClass('open'))
-				$(element).find('.sub-menu').removeClass('open');
+				$(element).find('.sub-menu').removeClass('open').navHide(animationSpeed);
 			else {
-				nav_has_sub_menus.not($(this).parents('.has-sub-menu')).find('.sub-menu').removeClass('open');
-				$(element).find('> .sub-menu').addClass('open');	
+				nav_has_sub_menus.not($(this).parents('.has-sub-menu')).find('.sub-menu').removeClass('open').navHide(animationSpeed);
+				$(element).find('> .sub-menu').addClass('open').navShow(animationSpeed);
 			}						
 		});
 
 		$(window).on("click", function(e){
 			if($(e.target).parents('#menu-rd').length == 0){
-				$('.sub-menu').removeClass('open');	
+				$('.sub-menu').removeClass('open').navHide(animationSpeed);	
 			}
 		})
 	} else {
-		$(element).on({mouseover: function(){
+		$(element).hover(function(){
 			clearTimeout(hover_timer);
-			$('> .sub-menu', element).addClass('open');
-		}, mouseout : function(){
+			if(!$('> .sub-menu', element).hasClass('open'))
+				$('> .sub-menu', element).addClass('open').navShow(animationSpeed);
+		}, function(){
 			hover_timer = setTimeout(function(){
-				$('> .sub-menu', element).removeClass('open');
-			}, 100); //0 for closing immediately
-		}}).find('> .show-sub-menu').on({focus : function(){
-			nav_has_sub_menus.not($(this).parents('.has-sub-menu')).find('.sub-menu').removeClass('open');
-			$('> .sub-menu', element).addClass('open');
-		}});
-
-		var all_f = $('#menu-rd a').add('#menu-rd .show-sub-menu');
-		all_f.on({blur : function(){
-			setTimeout(function(){
-				if(all_f.filter(":focus").length==0)
-					$('.sub-menu').removeClass('open');		
-			}, 50);
-		}});
+				$('> .sub-menu', element).removeClass('open').navHide(animationSpeed);
+			}, hideSubMenuDelay);
+		});
 	}	
 });
+
+if(!$('html').hasClass('touch')){
+
+	$('#menu-rd').find('.show-sub-menu').on({focus : function(){
+		$(this).siblings('.sub-menu').addClass('open').navShow(animationSpeed);;
+	}});
+
+	$('#menu-rd').find('.sub-menu').each(function(index, element){
+		$(this).find('> li:last-child > a').on('blur', function(){
+			$(element).removeClass('open').navHide(animationSpeed);
+		});
+	});
+}
 
 }(jQuery, this, this.document));
